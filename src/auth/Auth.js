cfa085761e914f32d4322d472bca3c5d06131c43
Eugene <eugzubv@gmail.com>
2020-10-14 20:05:17 +0300

    remove redundant class

--- src/auth/Auth.js
+++ src/auth/Auth.js
@@ -4,17 +4,13 @@ import { AuthWrapper } from "./styles.js";
 import Login from "./Login";
 import Register from "./Register";
 
-export default class Auth extends Component {
-  state = {};
-
-  render() {
-    return (
-      <AuthWrapper>
-        <h1>Login</h1>
-        <Login />
-        <h2>Registration</h2>
-        <Register />
-      </AuthWrapper>
-    );
+export const Auth = props => (
+  <AuthWrapper>
+    <h1>Login</h1>
+    <Login />
+    <h2>Registration</h2>
+    <Register />
+  </AuthWrapper>
+);
   }
 }
